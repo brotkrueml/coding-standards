bd3f70577c5dba5c7315a8a8f3b3df92791d06b0
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2022-05-16 17:45:58 +0200

    [!!!][TASK] Use new ECSConfig syntax

--- ecs.php
+++ ecs.php
@@ -2,17 +2,12 @@
 
 declare (strict_types=1);
 
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\EasyCodingStandard\ValueObject\Option;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(__DIR__ . '/config/common.php');
+return static function (ECSConfig $config): void {
+    $config->import(__DIR__ . '/config/common.php');
 
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(
-        Option::PATHS,
-        [
-            __DIR__ . '/config',
-        ]
-    );
+    $config->paths([
+        __DIR__ . '/config',
+    ]);
 };
